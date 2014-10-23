e5b36fda6afd01c3befee7ed3b77997cda189cb1
MEHRANI ARDALAN <3402426@ari-31-308-01.infop6.jussieu.fr>
2014-10-23 13:20:22 +0200

    adding stuff
    Signed-off-by: MEHRANI ARDALAN <3402426@ari-31-308-01.infop6.jussieu.fr>

--- Information Retrival/TP1/TP1-Indexing/src/Main.java
+++ Information Retrival/TP1/TP1-Indexing/src/Main.java
@@ -1,6 +1,7 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.Set;
@@ -24,15 +25,21 @@ public class Main {
 			Parser_cisi arda_parser = new Parser_cisi();
 			Stemmer stem = new Stemmer();
 
-			HashMap<Integer,Integer> docs = new HashMap<Integer,Integer>();
+			HashMap<Integer,String> docs = new HashMap<Integer,String>();
 			HashMap<String,Integer> stems = new HashMap<String, Integer>();
 			HashMap<Integer,String> docFrom = new HashMap<Integer, String>();
 
 			int pos = 0;
 
 
-			String index_index = "/home/arda-mint/Desktop/Link to M2/Information Retrival/TP1/Index/index_index.txt";
-			String index_inverted = "/home/arda-mint/Desktop/Link to M2/Information Retrival/TP1/Index/index_inverted.txt";
+			//			String index_index = "/home/arda-mint/Desktop/Link to M2/Information Retrival/TP1/Index/index_index.txt";
+			//			String index_inverted = "/home/arda-mint/Desktop/Link to M2/Information Retrival/TP1/Index/index_inverted.txt";
+
+			String index_index = "/users/Etu6/3402426/Documents/M2/Information Retrival/TP1/Index/index_index.txt";
+			String index_inverted = "/users/Etu6/3402426/Documents/M2/Information Retrival/TP1/Index/index_inverted.txt";
+
+
+
 
 			RandomAccessFile index= new RandomAccessFile(index_index, "rw");
 			RandomAccessFile inverted = new RandomAccessFile(index_inverted, "rw");
@@ -43,30 +50,84 @@ public class Main {
 
 				Document doc = arda_parser.nextDocument();
 
-				//						System.out.println("DOC ID :"+doc.id);
-				//						System.out.println("DOC FROM :"+ doc.from);
+
 				stems = stem.porterStemmerHash(doc.text);
-
 				remove_star(stems);
+				
+				System.out.println(stems.toString());
 
-				String hash2string = stems.toString();
+				String stems_string = stems.toString().replaceAll("{", "");
+
+				String regex = ",\\s"; 
+				String[] splitted = stems_string.split(regex);//getting : "word=doc_freq"
+				System.out.println(splitted.length);
+
+
+				for (int j=0;j<splitted.length;j++){
+
+					String word = splitted[j].split("=")[0]; //getting : "word"
+					String doc_freq = splitted[j].split("=")[1]; // getting : "doc_freq"
+
+
+					String one_line = word+":"+doc.id+"="+doc_freq+";";
+					System.out.println(one_line);
+
+
+
+				}
+
+
+
+
+
+
+
+
+
+				//Filling docFrom : Key = doc.id, Value = "Position:Length"
+				docFrom.put(doc.id, doc.from);
+
+
+
+
+
+				// Filling docs : Key = doc.id, Value = "Position:Length" // in the index file
+				String hash2string = doc.id+":"+stems.toString()+"\n";
+
+				//				System.out.println(hash2string);
 				byte[] string_byte = hash2string.getBytes("UTF-8");
-				System.out.println(string_byte.length);
+				//				System.out.println(string_byte.length);
 				int longueur = string_byte.length;
 
-				docs.put(pos,longueur);
-				System.out.println(docs.entrySet());
+				docs.put(doc.id,pos+":"+longueur);
+				//				System.out.println(docs.entrySet());
+
+				index.seek(pos);
+				index.write(string_byte);
 
 				pos = pos + longueur;
+
+
+
+
+
+
 			}//end for
+			//
+			//System.out.println(docs.entrySet());
+			//System.out.println(docFrom.entrySet());
 
 
 
 			//					while (arda_parser.file.length()!=arda_parser.file.getFilePointer()) {
-			for (int i=0; i<2;i++){
-
-
-			}//end for
+			//			for (int i=0; i<1;i++){
+			//				
+			//				index.seek(0);
+			//				byte[] b = 
+			////					index.write(b);
+			//				
+			//				
+			//			}//end for
 
 		}//End try
 
